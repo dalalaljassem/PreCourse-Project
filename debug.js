1789d6d5bdda83d88935a1e931cd6b451c981d12
dalal aljassem <deealjassem@dalals-MacBook-Pro.local>
2022-03-22 19:41:33 +0300

    3 fns done

--- debug.js
+++ debug.js
@@ -14,10 +14,10 @@ function generateIntegersArray(firstNumber, lastNumber) {
     integersArray.push(firstNumber);
     lastNumber++;
 
-  return IntegersArray;
+  return integersArray;
 }
 
-// console.log(generateIntegersArray(3, 7));
+console.log(generateIntegersArray(3, 7));
 
 /**
  * noZeroes(numberString):
@@ -31,17 +31,17 @@ function generateIntegersArray(firstNumber, lastNumber) {
  *
  */
 function noZeroes(numberString) {
-  whil (numberString.startsWith("0")) {
+  while (numberString.startsWith("0")) {
     numberString.slice(0, -1);
   }
 
-  while (numberString.endswith("0") {
+while (numberString.endswith("0"){
     numberString.slice[1];
   }
   numberString;
 }
 
-// console.log(noZeroes("001100"));
+console.log(noZeroes("001100"));
 
 module.exports = {
   generateIntegersArray,
--- functions.js
+++ functions.js
@@ -18,119 +18,125 @@
  * sumOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]) -> 25
  * sumOdds([3, 7, 8, 15, 2, 1, 13]) -> 39
  */
-function sumOdds(numbers) {
-  // Your code here
-}
-// console.log(sumOdds([3, 7, 8, 15, 2, 1, 13]));
-
-/**
- * characterCount(string, c):
- * - receives a string and a character
- * - returns the number of times `c` occurs in the string
- * - note: this has to be case-insensitive
- *
- * e.g.
- * characterCount("Michael Stephenson", "e") -> 3
- * characterCount("Character Count is clever", "c") -> 4
- *
- * Hint: You need to turn the string into an array first
- * Another Hint: Use string methods to make it case-insensitive
- */
-function characterCount(string, c) {
-  // Your code here
-}
-// console.log(characterCount("Character Count is clever", "c"));
-
-/**
- * largestIncrement(numbers):
- * - receives an array of numbers
- * - returns the largest difference between two consecutive numbers in the array
- * - see example below for clarification
- * - assume all differences are positive numbers
- * e.g.
- * largestIncrement([1, 3, 7, 9, 12]) -> 4
- * 3 - 1 = 2
- * 7 - 3 = 4
- * 9 - 7 = 2
- * 12 - 9 = 3
- * largest: 4
- *
- * largestIncrement([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> 545
- */
-function largestIncrement(numbers) {
-  // Your code here
-}
-// console.log(largestIncrement([11, 35, 52, 14, 56, 601, 777, 888, 999]));
-
-/**
- * afterX(numbers, x):
- * - receives an array of numbers, and a number `x`.
- * - returns an array of every number in `numbers` that occurs after `x`.
- *
- * - assume there are no duplicate numbers.
- *
- * e.g.
- * afterX([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) -> [4, 5, 6, 7, 8, 9]
- * afterX([11, 35, 52, 14, 56, 601, 777, 888, 999], 52) -> [14, 56, 601, 777, 888, 999]
- */
-function afterX(numbers, x) {
-  // Your code here
-}
-// console.log(afterX([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
-
-/**
- * abbreviate(firstName, lastName):
- * - receives a first name and a last name
- * - returns the initials in uppercase
- *
- * e.g.
- * abbreviate("Michael", "Singer") -> "MS"
- * abbreviate("jordan", "peterson") -> "JP"
- *
- * Hint: Use string method .toUpperCase()
- */
-function abbreviate(firstName, lastName) {
-  // Your code here
-}
-// console.log(abbreviate("miss", "Stephane"));
-
-/**
- * isUpperCase(string):
- * - receives a string
- * - returns true if the string is uppercase, false otherwise
- *
- * e.g.
- * isUpperCase("Mickey S") -> false
- * isUpperCase("JCREW") -> true
- *
- */
-function isUpperCase(string) {
-  // Your code here
-}
-
-// console.log(isUpperCase("JCREW"));
-
-/**
- * elementInArray(numbers, x):
- * - receives an array of numbers, and a number `x`.
- * - returns true if `x` is found in the array, false otherwise
- *
- * e.g.
- * elementInArray([5, 6, 7], 6) -> true
- * elementInArray([5, 6, 7], 8) -> false
- *
- */
-function elementInArray(numbers, x) {
-  // Your code here
-}
-// console.log(elementInArray([5, 6, 7], 8));
-
-module.exports = {
-  sumOdds,
-  characterCount,
-  largestIncrement,
-  afterX,
-  abbreviate,
-  isUpperCase,
-  elementInArray,
-};
+ function sumOdds(numbers) {
+  let oddNum = numbers.filter((num) => num % 2 !== 0);
+  return oddNum.map((num) => num + num);
+  }
+  
+    console.log(sumOdds([3, 7, 8, 15, 2, 1, 13]));
+    
+    /**
+     * characterCount(string, c):
+     * - receives a string and a character
+     * - returns the number of times `c` occurs in the string
+     * - note: this has to be case-insensitive
+     *
+     * e.g.
+     * characterCount("Michael Stephenson", "e") -> 3
+     * characterCount("Character Count is clever", "c") -> 4
+     *
+     * Hint: You need to turn the string into an array first
+     * Another Hint: Use string methods to make it case-insensitive
+     */
+    function characterCount(string, c) {
+      // turn the string into an array first
+      let ch = `c`;
+      const stringArray = string.split(ch).length;
+      console.log(stringArray);
+    }
+    console.log(characterCount("Character Count is clever", "c"));
+    
+    /**
+     * largestIncrement(numbers):
+     * - receives an array of numbers
+     * - returns the largest difference between two consecutive numbers in the array
+     * - see example below for clarification
+     * - assume all differences are positive numbers
+     * e.g.
+     * largestIncrement([1, 3, 7, 9, 12]) -> 4
+     * 3 - 1 = 2
+     * 7 - 3 = 4
+     * 9 - 7 = 2
+     * 12 - 9 = 3
+     * largest: 4
+     *
+     * largestIncrement([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> 545
+     */
+    function largestIncrement(numbers) {
+      return numbers.slice(1).map(function(n, i) { return n - numbers[i]; });
+      //if 
+  }
+    console.log(largestIncrement([11, 35, 52, 14, 56, 601, 777, 888, 999]));
+    
+    /**
+     * afterX(numbers, x):
+     * - receives an array of numbers, and a number `x`.
+     * - returns an array of every number in `numbers` that occurs after `x`.
+     *
+     * - assume there are no duplicate numbers.
+     *
+     * e.g.
+     * afterX([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) -> [4, 5, 6, 7, 8, 9]
+     * afterX([11, 35, 52, 14, 56, 601, 777, 888, 999], 52) -> [14, 56, 601, 777, 888, 999]
+     */
+    function afterX(numbers, x) {
+      // Your code here
+    }
+    // console.log(afterX([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+    
+    /**
+     * abbreviate(firstName, lastName):
+     * - receives a first name and a last name
+     * - returns the initials in uppercase
+     *
+     * e.g.
+     * abbreviate("Michael", "Singer") -> "MS"
+     * abbreviate("jordan", "peterson") -> "JP"
+     *
+     * Hint: Use string method .toUpperCase()
+     */
+    function abbreviate(firstName, lastName) {
+      // Your code here
+    }
+    // console.log(abbreviate("miss", "Stephane"));
+    
+    /**
+     * isUpperCase(string):
+     * - receives a string
+     * - returns true if the string is uppercase, false otherwise
+     *
+     * e.g.
+     * isUpperCase("Mickey S") -> false
+     * isUpperCase("JCREW") -> true
+     *
+     */
+    function isUpperCase(string) {
+      // Your code here
+    }
+    
+    // console.log(isUpperCase("JCREW"));
+    
+    /**
+     * elementInArray(numbers, x):
+     * - receives an array of numbers, and a number `x`.
+     * - returns true if `x` is found in the array, false otherwise
+     *
+     * e.g.
+     * elementInArray([5, 6, 7], 6) -> true
+     * elementInArray([5, 6, 7], 8) -> false
+     *
+     */
+    function elementInArray(numbers, x) {
+      // Your code here
+    }
+    // console.log(elementInArray([5, 6, 7], 8));
+    
+    module.exports = {
+      sumOdds,
+      characterCount,
+      largestIncrement,
+      afterX,
+      abbreviate,
+      isUpperCase,
+      elementInArray,
+    };
